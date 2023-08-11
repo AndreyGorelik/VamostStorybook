461fb0b44d75375cb341b22ed000fca206cd2ca2
AndreyGorelik <bublic007@inbox.ru>
2023-08-11 17:01:33 +0300

    extras changed

--- app.config.js
+++ app.config.js
@@ -17,4 +17,10 @@ export default ({ config }) => ({
       },
     },
   },
+  updates: {
+    url: 'https://u.expo.dev/67ccfb62-b63f-4f49-bfd9-9167440ad498',
+  },
+  runtimeVersion: {
+    policy: 'appVersion',
+  },
 });
