be954f385f4098bfaf30c298bb1665065541b12e
HenoL-rgb <sasha.diretic@gmail.com>
2023-09-04 15:08:51 +0300

    some optimization to post list

--- src/shared/ui/postsList/postsList.component.tsx
+++ src/shared/ui/postsList/postsList.component.tsx
@@ -1,11 +1,13 @@
 import { PostCard } from '@shared/ui/postCard';
-import { View, FlatList } from 'react-native';
+import { View, FlatList, ActivityIndicator } from 'react-native';
 
 import { createStyles } from './postsList.styles';
 import { PostListProps } from './postsList.types';
+import useTheme from '@shared/hooks/useTheme.hook';
 
 export default function PostsList({ list }: PostListProps) {
   const styles = createStyles();
+  const theme = useTheme();
 
   return (
     <FlatList
@@ -14,6 +16,8 @@ export default function PostsList({ list }: PostListProps) {
       keyExtractor={(item) => `${item.id}`}
       ItemSeparatorComponent={() => <View style={styles.separator}></View>}
       contentContainerStyle={styles.wrapper}
+      ListEmptyComponent={() => <ActivityIndicator size={24} color={theme.colors.primary} />}
+      maxToRenderPerBatch={5}
     />
   );
 }
