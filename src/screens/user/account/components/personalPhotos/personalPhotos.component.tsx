ac15428d134fd9b4ef439d2e805bec027e48bb48
HenoL-rgb <sasha.diretic@gmail.com>
2023-09-07 10:09:34 +0300

    cleaning

--- src/screens/user/account/components/personalPhotos/personalPhotos.component.tsx
+++ src/screens/user/account/components/personalPhotos/personalPhotos.component.tsx
@@ -1,7 +1,4 @@
-import { useAppSelector } from '@shared/hooks/redux.hook';
 import useTheme from '@shared/hooks/useTheme.hook';
-import * as ImagePicker from 'expo-image-picker';
-import { useState } from 'react';
 import { View } from 'react-native';
 
 import { PhotoContainer } from '../photoContainer';
@@ -11,51 +8,6 @@ import { createStyles } from './personalPhotos.styles';
 export default function PersonalPhotos() {
   const theme = useTheme();
   const styles = createStyles(theme);
-  const [isLoading, setIsLoading] = useState<number | null>(null);
-  const [status, requestPermission] = ImagePicker.useCameraPermissions();
-  const { images: savedImages } = useAppSelector((state) => state.userSlice);
-  const images = savedImages.map((image, index) => ({ id: index, image }));
-
-  const pickImage = async (id: number) => {
-    if (!status?.granted) {
-      await requestPermission();
-    }
-    setIsLoading(id);
-
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: false,
-      quality: 1,
-      allowsMultipleSelection: true,
-      base64: true,
-    });
-
-    // if (!result.canceled) {
-    //   if (id <= images.length) {
-    //     setImages(
-    //       result.assets.map((asset) => ({
-    //         uri: asset.uri,
-    //         imageData: 'data:image/jpeg;base64,' + asset.base64,
-    //       }))
-    //     );
-    //   } else {
-    //     setImages([
-    //       ...images,
-    //       ...result.assets
-    //         .map((asset) => ({
-    //           uri: asset.uri,
-    //           imageData: 'data:image/jpeg;base64,' + asset.base64,
-    //         }))
-    //         .slice(0, 6 - images.length),
-    //     ]);
-    //   }
-    // }
-    setIsLoading(null);
-  };
-
-  function handleDelete(imageIndex: number) {
-    //setImages(images.filter((value, index) => index !== imageIndex - 1));
-  }
 
   return (
     <View style={styles.wrapper}>
