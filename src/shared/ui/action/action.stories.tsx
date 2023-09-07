cc9e043a9ff0da9df749a52f15afea238dc29d3d
HenoL-rgb <57561622+HenoL-rgb@users.noreply.github.com>
2023-09-07 14:22:51 +0300

    Update action.stories.tsx

--- src/shared/ui/action/action.stories.tsx
+++ src/shared/ui/action/action.stories.tsx
@@ -1,5 +1,6 @@
 import { MaterialIcons, AntDesign, Ionicons } from '@expo/vector-icons';
 import { View } from 'react-native';
+import useTheme from '@shared/hooks/useTheme.hook';
 
 import Action from './action.component';
 
@@ -9,6 +10,7 @@ export default {
 };
 
 const Template = () => {
+  const theme = useTheme();
   return (
     <View
       style={{
@@ -20,25 +22,25 @@ const Template = () => {
       }}
     >
       <Action
-        Icon={<MaterialIcons size={26} name="share" color="white" />}
+        Icon={<MaterialIcons size={26} name="share" color={theme.colors.secondary} />}
         title="Share"
         onPress={() => {
         }}
       />
       <Action
-        Icon={<AntDesign size={26} name="heart" color="white" />}
+        Icon={<AntDesign size={26} name="heart" color={theme.colors.secondary} />}
         title="Favorite"
         onPress={() => {
         }}
       />
       <Action
-        Icon={<Ionicons name="chatbubble-ellipses" size={24} color="white" />}
+        Icon={<Ionicons name="chatbubble-ellipses" size={24} color={theme.colors.secondary} />}
         title="Chat"
         onPress={() => {
         }}
       />
       <Action
-        Icon={<MaterialIcons name="business-center" size={24} color="white" />}
+        Icon={<MaterialIcons name="business-center" size={24} color={theme.colors.secondary} />}
         title="Request"
         onPress={() => {
         }}
