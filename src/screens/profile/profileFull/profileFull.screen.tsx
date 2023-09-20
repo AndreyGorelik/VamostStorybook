6e83fdbb6783ab0c2d523506ab787991f0b36551
AndreyGorelik <bublic007@inbox.ru>
2023-09-20 16:30:10 +0200

    gallery with carousel and grid like view

--- src/screens/profile/profileFull/profileFull.screen.tsx
+++ src/screens/profile/profileFull/profileFull.screen.tsx
@@ -2,6 +2,7 @@ import Background from '@assets/images/postCardImages/postCardMainPhoto.jpeg';
 import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { useAppDispatch, useAppSelector } from '@shared/hooks/redux.hook';
 import useTheme from '@shared/hooks/useTheme.hook';
+import PhotoGallery from '@shared/ui/photoGallery/photoGallery.component';
 import Text from '@shared/ui/text/text.component';
 import Constants from 'expo-constants';
 import { useLocalSearchParams } from 'expo-router';
@@ -11,7 +12,53 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 import { getProfile } from 'src/store/slices/profileSlice';
 
 import { createStyles } from './profileFull.styles';
-
+const DATA = [
+  {
+    id: '1',
+    imageUrl:
+      'https://i.kym-cdn.com/entries/icons/facebook/000/027/852/Screen_Shot_2018-12-12_at_1.02.39_PM.jpg',
+  },
+  {
+    id: '2',
+    imageUrl:
+      'https://i.kym-cdn.com/entries/icons/facebook/000/027/852/Screen_Shot_2018-12-12_at_1.02.39_PM.jpg',
+  },
+  {
+    id: '3',
+    imageUrl:
+      'https://i.kym-cdn.com/entries/icons/facebook/000/027/852/Screen_Shot_2018-12-12_at_1.02.39_PM.jpg',
+  },
+  {
+    id: '4',
+    imageUrl:
+      'https://i.kym-cdn.com/entries/icons/facebook/000/027/852/Screen_Shot_2018-12-12_at_1.02.39_PM.jpg',
+  },
+  {
+    id: '5',
+    imageUrl:
+      'https://i.kym-cdn.com/entries/icons/facebook/000/027/852/Screen_Shot_2018-12-12_at_1.02.39_PM.jpg',
+  },
+  {
+    id: '6',
+    imageUrl:
+      'https://i.kym-cdn.com/entries/icons/facebook/000/027/852/Screen_Shot_2018-12-12_at_1.02.39_PM.jpg',
+  },
+  {
+    id: '7',
+    imageUrl:
+      'https://i.kym-cdn.com/entries/icons/facebook/000/027/852/Screen_Shot_2018-12-12_at_1.02.39_PM.jpg',
+  },
+  {
+    id: '8',
+    imageUrl:
+      'https://i.kym-cdn.com/entries/icons/facebook/000/027/852/Screen_Shot_2018-12-12_at_1.02.39_PM.jpg',
+  },
+  {
+    id: '9',
+    imageUrl:
+      'https://i.kym-cdn.com/entries/icons/facebook/000/027/852/Screen_Shot_2018-12-12_at_1.02.39_PM.jpg',
+  },
+];
 export default function ProfileFull() {
   const params = useLocalSearchParams();
   const { id } = params;
@@ -24,8 +71,6 @@ export default function ProfileFull() {
 
   const theme = useTheme();
   const styles = createStyles(theme);
-
-  console.log(typeof profile.images[0].imageUrl);
 
   const shareProfileLink = async () => {
     const ip = Constants.linkingUri.split('//')[1].split(':')[0];
@@ -69,58 +114,19 @@ export default function ProfileFull() {
           <Text variant="h5">Birthdate</Text>
           <Text>{profile.birthdate}</Text>
         </View>
-        {JSON.parse(profile.sexualOrientation.toString()).isShown && (
+        {/* {JSON.parse(profile?.sexualOrientation?.toString()).isShown && (
           <View style={styles.infoRow}>
             <Text variant="h5">Sexual orientation</Text>
-            <Text>{JSON.parse(profile.sexualOrientation.toString()).value}</Text>
+            <Text>{JSON.parse(profile?.sexualOrientation.toString()).value}</Text>
           </View>
         )}
-        {JSON.parse(profile.gender.toString()).isShown && (
+        {JSON.parse(profile?.gender?.toString()).isShown && (
           <View style={styles.infoRow}>
             <Text variant="h5">Gender</Text>
-            <Text>{JSON.parse(profile.gender.toString()).value}</Text>
+            <Text>{JSON.parse(profile?.gender.toString()).value}</Text>
           </View>
-        )}
-        <View style={{ flexDirection: 'row', flexWrap: 'wrap', gap: 5 }}>
-          {profile.images.slice(0,2).map((item, index, array) => {
-            if (index === 0 && array.length > 1) {
-              return (
-                <Image
-                  key={item.id}
-                  source={{ uri: item.imageUrl }}
-                  style={{ width: '65%', height: 170, borderRadius: 15, objectFit: 'cover' }}
-                />
-              );
-            }
-
-            if (index === 0 && array.length === 1) {
-              return (
-                <Image
-                  key={item.id}
-                  source={{ uri: item.imageUrl }}
-                  style={{ width: '100%', height: 170, borderRadius: 15, objectFit: 'cover' }}
-                />
-              );
-            }
-
-            if (index < 6 && index !== 0) {
-              return (
-                <Image
-                  key={item.id}
-                  source={{ uri: item.imageUrl }}
-                  style={{
-                    flex: 1,
-                    minWidth: '30%',
-                    height: 170,
-                    borderRadius: 15,
-                    objectFit: 'cover',
-                  }}
-                />
-              );
-            }
-            return null;
-          })}
-        </View>
+        )} */}
+        <PhotoGallery images={DATA} />
       </View>
     </ScrollView>
   );
---
+++ src/shared/ui/photoGallery/photoGallery.component.tsx
@@ -0,0 +1,100 @@
+import useTheme from '@shared/hooks/useTheme.hook';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useState } from 'react';
+import { Image, ImageBackground, TouchableOpacity, View } from 'react-native';
+
+import Text from '../text/text.component';
+
+import ModalGallery from './components/modalGallery/modalGallery.component';
+import { createStyles } from './photoGallery.styles';
+import { PhotoGalleryProps } from './photoGallery.types';
+
+const MAX_IMAGES = 6;
+
+function PhotoGallery({ images }: PhotoGalleryProps) {
+  const theme = useTheme();
+  const styles = createStyles(theme);
+  const [openModalGallery, setOpenModalGallery] = useState(false);
+
+  if (!images) return null;
+
+  return (
+    <>
+      <View style={{ flexDirection: 'row', flexWrap: 'wrap', gap: 5 }}>
+        {images.map((item, index, array) => {
+          const isTheOnlyOneImage = index === 0 && array.length === 1;
+          const isTheFirstImage = index === 0 && array.length > 1;
+          const notTheFirstImage = index < MAX_IMAGES && index !== 0;
+          const isLastImage = index === array.length - 1;
+
+          if (isTheOnlyOneImage) {
+            return (
+              <Image
+                key={item.id}
+                source={{ uri: item.imageUrl }}
+                style={styles.isTheOnlyOneImage}
+              />
+            );
+          }
+
+          if (isTheFirstImage) {
+            return (
+              <Image key={item.id} source={{ uri: item.imageUrl }} style={styles.isTheFirstImage} />
+            );
+          }
+
+          if (notTheFirstImage && !isLastImage) {
+            return (
+              <Image
+                key={item.id}
+                source={{ uri: item.imageUrl }}
+                style={styles.notTheFirstImage}
+              />
+            );
+          }
+
+          if (isLastImage) {
+            if (array.length > MAX_IMAGES) {
+              return (
+                <TouchableOpacity
+                  key={item.id}
+                  style={styles.isLastImage}
+                  activeOpacity={0.8}
+                  onPress={() => setOpenModalGallery(true)}
+                >
+                  <ImageBackground
+                    imageStyle={styles.isLastImage}
+                    source={{
+                      uri: item.imageUrl,
+                    }}
+                    style={styles.flex}
+                  >
+                    <LinearGradient
+                      colors={[theme.colors.gradientFinish, theme.colors.gradientFinish]}
+                      style={styles.linearGradient}
+                    >
+                      <Text variant="h2" color={theme.colors.secondary}>
+                        +{(array.length - MAX_IMAGES).toString()}
+                      </Text>
+                    </LinearGradient>
+                  </ImageBackground>
+                </TouchableOpacity>
+              );
+            } else {
+              return (
+                <Image key={item.id} source={{ uri: item.imageUrl }} style={styles.isLastImage} />
+              );
+            }
+          }
+
+          return null;
+        })}
+      </View>
+      {openModalGallery && (
+        <ModalGallery close={() => setOpenModalGallery(false)} images={images} />
+      )}
+    </>
+  );
+}
+
+export default PhotoGallery;
